dd300c5e6f9bebd0e790bd9eacf0f1b409153db2
Andrew Sun <me@andrewsun.com>
2017-06-27 15:10:11 -0400

    CSSUnit for better autocompletion

--- src/CSSNumber.ts
+++ src/CSSNumber.ts
@@ -1,29 +1,37 @@
+export type CSSUnit = "em" | "px" | "rem" | "%" | "vh" | "vw" | "vi" | "vb" | "vmin" | "vmax" | "mm" | "q" | "cm" |
+	"in" | "pt" | "pc" | "ex" | "cap" | "ch" | "ic" | "lh" | "rlh" | "mozmm" | "deg" | "grad" | "rad" | "turn" | "fr" |
+	"Hz" | "kHz" | "dpi" | "dpcm" | "dppx" | "s" | "ms" | string;
+
 export default class CSSNumber {
 	public value: number;
-	public unit: string;
+	public unit: CSSUnit;
 
-	public constructor(value: number, unit: string) {
+	public constructor(value: number, unit: CSSUnit) {
 		this.value = value;
 		this.unit = unit;
 	}
 
-	public add(num: CSSNumber, unit: string = this.unit) {
+	public add(num: CSSNumber, unit: CSSUnit = this.unit) {
 		return new CSSNumber(this.value + num.value, unit);
 	}
 
-	public sub(num: CSSNumber, unit: string = this.unit) {
+	public sub(num: CSSNumber, unit: CSSUnit = this.unit) {
 		return new CSSNumber(this.value - num.value, unit);
 	}
 
-	public mul(num: CSSNumber, unit: string = this.unit) {
+	public mul(num: CSSNumber, unit: CSSUnit = this.unit) {
 		return new CSSNumber(this.value * num.value, unit);
 	}
 
-	public div(num: CSSNumber, unit: string = this.unit) {
+	public div(num: CSSNumber, unit: CSSUnit = this.unit) {
 		return new CSSNumber(this.value / num.value, unit);
 	}
 
 	public toString() {
+		if (this.value === 0) {
+			return "0";
+		}
+
 		// round it to a few decimal places
 		return Math.round(this.value * 100000) / 100000 + this.unit;
 	}
