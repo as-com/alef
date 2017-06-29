2ee40fab3c5db32ec85926fcfe8887ec83b352a8
Andrew Sun <me@andrewsun.com>
2017-06-28 22:52:39 -0400

    remove cjs (non-es2015) modules

--- gulpfile.js
+++ gulpfile.js
@@ -3,29 +3,16 @@ const ts = require("gulp-typescript");
 const sourceMaps = require("gulp-sourcemaps");
 const del = require("del");
 
-const tsProjectCJS = ts.createProject("tsconfig.json", {
-	module: "commonjs"
-});
-
 const tsProjectES = ts.createProject("tsconfig.json", {
 	module: "es6"
 });
 
-gulp.task("default", ["lib", "es"]);
+gulp.task("default", ["es"]);
 
 gulp.task("clean", function () {
 	return del([
-		"lib/**/*",
 		"es/**/*"
 	]);
-});
-
-gulp.task("lib", ["clean"], function() {
-	tsProjectCJS.src()
-		.pipe(sourceMaps.init())
-		.pipe(tsProjectCJS())
-		.pipe(sourceMaps.write("."))
-		.pipe(gulp.dest("lib"));
 });
 
 gulp.task("es", ["clean"], function () {
